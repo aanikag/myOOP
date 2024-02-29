272e82694397a086020db56baf8968b82544b11d
aanikagarre <aanikagarre@aanikas-mbp.wifi.uc.edu>
2024-02-29 13:27:44 -0500

    Second commit

--- InClass_20240227/vehiclePackage/convertibleClass.py
+++ InClass_20240227/vehiclePackage/convertibleClass.py
@@ -1,1 +1,10 @@
 #convertible Class
+from vehiclePackage.Car import Car;       
+
+class Convertible(Car):
+    def __init__(self, type, make, model, doors):
+        self.doors = doors;
+        Car.__init__(self, type, make, model);
+        
+    def printDoors(self):
+        print(self.foors);
